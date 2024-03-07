337ffd97088ca6a23d0d21fee1e3474aa3baca65
Christopher Kirathe <chrisk720@Christophers-MacBook-Pro.local>
2024-03-07 10:55:45 +0300

    Done with Lab thank God

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,63 @@
 #!/usr/bin/env python3
 
+
+# def test_has_brand_and_size(self):
+#         '''has the brand and size passed to __init__, and values can be set to new instance.'''
+#         stan_smith = Shoe("Adidas", 9)
+#         assert(stan_smith.brand == "Adidas")
+#         assert(stan_smith.size == 9)
+
+
+# def test_requires_int_size(self):
+#         '''prints "size must be an integer" if size is not an integer.'''
+#         stan_smith = Shoe("Adidas", 9)
+#         captured_out = io.StringIO()
+#         sys.stdout = captured_out
+#         stan_smith.size = "not an integer"
+#         sys.stdout = sys.__stdout__
+#         assert captured_out.getvalue() == "size must be an integer\n"
+
+
+
+# def test_can_cobble(self):
+#         '''says that the shoe has been repaired.'''
+#         stan_smith = Shoe("Adidas", 9)
+#         captured_out = io.StringIO()
+#         sys.stdout = captured_out
+#         stan_smith.cobble()
+#         sys.stdout = sys.__stdout__
+#         assert(captured_out.getvalue() == "Your shoe is as good as new!\n")
+    
+
+
+#  def test_cobble_makes_new(self):
+#         '''creates an attribute on the instance called 'condition' and set equal to 'New' after repair.'''
+#         stan_smith = Shoe("Adidas", 9)
+#         stan_smith.cobble()
+#         assert(stan_smith.condition == "New")
+        
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.brand = brand
+        self.size = size
+
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self,size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print('size must be an integer')
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = 'New'
+
+stan_smith = Shoe('Adidas',9)
+print(stan_smith.size)
+
+    
